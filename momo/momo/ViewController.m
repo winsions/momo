d806959e7a876819f663ee5a5932a12d255c6746
oneshunzi <222@qq.com>
2015-12-12 14:46:59 +0800

    aaa

--- momo/momo/ViewController.m
+++ momo/momo/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // 实验实验!!!
 }
 
 - (void)didReceiveMemoryWarning {
